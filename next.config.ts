7513e2ec078fec88e0e58e07a6f60ae2f57af3ca
haula-dv <hau.la.dv@gmail.com>
2025-12-26 14:01:03 +0700

    feat: add EvmProvider for Web3Onboard wallet connections with injected and WalletConnect modules, and expose APP_URL in next.config.ts.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  env: {
+    APP_URL: process.env.APP_URL,
+  },
 };
 
 export default nextConfig;
